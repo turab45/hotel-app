cad4bd5cf9eed99e74e5c31bc57f0edae75a0862
turab45 <turabbajeer202@gmail.com>
2021-09-26 21:44:14 +0500

    add swagger ui

--- src/main/java/com/hotel/api/DemoApplication.java
+++ src/main/java/com/hotel/api/DemoApplication.java
@@ -1,13 +1,48 @@
 package com.hotel.api;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
+@EnableSwagger2
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public Docket swaggerConfig() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+                .apis(RequestHandlerSelectors.basePackage("com.hotel.api.controllers"))
+                .paths(PathSelectors.any())
+                .build()
+				.apiInfo(apiInfo());
+	}
+	
+	public ApiInfo apiInfo() {
+		return new ApiInfo(
+				"Hotel app api",
+				"Api for hotel app",
+				"1.0",
+				"Free to use",
+				new Contact("Turab Bajeer", "https://github.com/turab45","turabbajeer202@gmail.com"),
+				"API Licence",
+				"https://github.com/turab45",
+				Collections.emptyList()
+				);
+	}
 }
